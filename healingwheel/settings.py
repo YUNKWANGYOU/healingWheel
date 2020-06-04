aa604f5c4e6854d25760f27637257b4e4752f4f1
1996yyk <1996yyk@gmail.com>
2020-06-04 10:50:46 +0900

    .

--- healingwheel/settings.py
+++ healingwheel/settings.py
@@ -120,8 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static_files')
-
+STATIC_URL = '/main/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
+
+SASS_PROCESSOR_ENABLED = True
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'main', 'static')
+SASS_OUTPUT_STYLE = 'compact'
